599ce835d3d873fbd00d04a5a8a757ce7615bf5a
KarthikSiddarth <iamkarthiksiddarth@outlook.com>
2018-08-14 16:58:19 +0530

    add comments

--- api/models/users.js
+++ api/models/users.js
@@ -1,8 +1,8 @@
-const mongoose = require('mongoose')
+const { Schema } = require('mongoose')
 
-const userSchema = mongoose.Schema({
+const userSchema = Schema({
   name: { type: String, required: true },
   address: { type: String, required: true },
-  pastOrders: [],
-  currentOrder: { item: String, quantity: Number, required: true }
+  pastOrders: [/*get from orders collection*/],
+  currentOrder: {}// get it from orders collection
 })
