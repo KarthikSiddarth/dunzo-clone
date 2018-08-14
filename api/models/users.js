6d86cb8a9781b80ac91a9b393c58e29398e0938d
KarthikSiddarth <iamkarthiksiddarth@outlook.com>
2018-08-14 15:32:01 +0530

    add api dir and inside models dir and define user schema

---
+++ api/models/users.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose')
+
+const userSchema = mongoose.Schema({
+  name: { type: String, required: true },
+  address: { type: String, required: true },
+  pastOrders: [],
+  currentOrder: { item: String, quantity: Number }
+})
