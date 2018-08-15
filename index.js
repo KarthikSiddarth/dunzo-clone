b0f1a76b4d57afdbc4afd3a32909291b7c723ee4
KarthikSiddarth <iamkarthiksiddarth@outlook.com>
2018-08-15 12:22:45 +0530

    add methods to handle router endpoint

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const express = require('express')
-
 const app = express()
+const bodyParser = require('body-parser')
+const orderRoute = require('./api/routes/orders')
 
 app.listen(8000, () => console.log('listening on port 8000'))
+
+app.use(bodyParser.json())
+app.use('/orders', orderRoute)
