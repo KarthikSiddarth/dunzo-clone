1446b9febe7e15433681fda0b2491096f4394a3d
Mukul Mahajan Singraur <rammmukul@outlook.com>
2018-08-15 12:40:55 +0530

    change position of midleware registration

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const bodyParser = require('body-parser')
 const orderRoute = require('./api/routes/orders')
 
-app.listen(8000, () => console.log('listening on port 8000'))
-
 app.use(bodyParser.json())
 app.use('/orders', orderRoute)
+
+app.listen(8000, () => console.log('listening on port 8000'))
