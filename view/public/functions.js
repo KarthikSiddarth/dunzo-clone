225d2e3f5cb97513fd4a209c76c871cd9560fe93
KarthikSiddarth <iamkarthiksiddarth@outlook.com>
2018-08-15 18:05:35 +0530

    add function to compose body for post request

--- view/public/functions.js
+++ view/public/functions.js
@@ -1,6 +1,19 @@
+function getPostOrderOptions (orderDescription) {
+  return {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ 'description': orderDescription })
+  }
+}
+
 function placeOrderFunction () {
   if (this.orderDescription) {
+    const url = 'http://localhost:8000/api/orders'
     this.showNoOrderWarning = false
+    let postOrderOptions = getPostOrderOptions(this.orderDescription)
+    fetch(url, postOrderOptions).then(data => data.json()).then(console.log)
     return
   }
   this.showNoOrderWarning = true
