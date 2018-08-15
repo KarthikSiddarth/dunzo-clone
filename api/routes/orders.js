d38dd432b2eadc675f277d63e17fcc767c955762
Mukul Mahajan Singraur <rammmukul@outlook.com>
2018-08-15 14:29:50 +0530

    fix order save

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -8,9 +8,7 @@ router.get('/', (req, res) => {
 
 router.post('/', (req, res) => {
   console.log(req.body)
-  const order = new Order({
-    description: req.description
-  })
+  const order = new Order(req.body)
   order.save()
     .then((result) => { res.status(200).json({message: 'order placed', result: result}) })
     .catch((err) => { res.status(500).json({message: 'order not placed', err: err}) })
