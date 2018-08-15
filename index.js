0b2431ca0e1e89303266bdbb757d5bfd1c1c7a15
KarthikSiddarth <iamkarthiksiddarth@outlook.com>
2018-08-15 13:16:40 +0530

    add functions for connecting mongo db

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
 const orderRoute = require('./api/routes/orders')
 
 app.listen(8000, () => console.log('listening on port 8000'))
 
 app.use(bodyParser.json())
 app.use('/orders', orderRoute)
+
+mongoose.connect('mongodb://localhost:27017/dunzoClone', { useNewUrlParser: true })
+
+let db = mongoose.connection
+
+db.on('open', () => { console.log('connected to dunzo clone db') })
