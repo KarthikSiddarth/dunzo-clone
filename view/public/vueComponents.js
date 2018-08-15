3fb47d99615d1d18be14d4f07d70e3b13fb96d25
KarthikSiddarth <iamkarthiksiddarth@outlook.com>
2018-08-15 18:06:43 +0530

    change event name from order to place order

--- view/public/vueComponents.js
+++ view/public/vueComponents.js
@@ -2,7 +2,7 @@ const placeOrderOptions = {
   template: `<div>
                 <input type="text" placeholder="Place your order" @input="$emit('input', $event.target.value)">
                 <p v-if="showNoOrderWarning">No order has been described</p>
-                <button @click="$emit('order')">place</button>
+                <button @click="$emit('place-order')">place</button>
              </div>`,
   props: ['showNoOrderWarning']
 }
