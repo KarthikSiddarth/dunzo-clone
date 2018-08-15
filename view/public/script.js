7ba10cd8070c7974e13edcb54cea26f81a36ce5b
KarthikSiddarth <iamkarthiksiddarth@outlook.com>
2018-08-15 16:43:14 +0530

    add properties and function to toggle warning and check input

--- view/public/script.js
+++ view/public/script.js
@@ -1,9 +1,24 @@
 const appOptions = {
   template: `<div>
                 <h3>Welcome, User</h3>
-                <place-order />
+                <place-order 
+                  v-model="orderDescription"
+                  @order="placeOrder"
+                  :showNoOrderWarning="showNoOrderWarning" />
              </div>`,
-  el: '#app'
+  el: '#app',
+  data: {
+    orderDescription: '',
+    showNoOrderWarning: false
+  },
+  methods: {
+    placeOrder: placeOrderFunction
+  },
+  watch: {
+    orderDescription () {
+      if (this.orderDescription) this.showNoOrderWarning = false
+    }
+  }
 }
 
 Vue.component('place-order', placeOrderOptions)
