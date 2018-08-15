f2152b0b5be3ef4d0c8dc1b7a7c5e74e260c0dfa
Mukul Mahajan Singraur <rammmukul@outlook.com>
2018-08-15 14:47:38 +0530

    add user to order

--- api/models/orders.js
+++ api/models/orders.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const orderSchema = new mongoose.Schema({
   description: { type: String, required: true },
-  // user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   runner: { type: mongoose.Schema.Types.ObjectId, ref: 'Runner' },
   status: { type: String, required: true, default: 'placed' }
 })
--- api/routes/orders.js
+++ api/routes/orders.js
@@ -1,14 +1,18 @@
 const router = require('express').Router()
 const Order = require('../models/orders')
+const User = require('../models/users')
 
 router.get('/', (req, res) => {
   console.log(req.url)
   res.status(200).json({message: 'Welcome, User'})
 })
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   console.log(req.body)
-  const order = new Order(req.body)
+  const order = new Order({
+    ...req.body,
+    user: (await User.findOne({ name: 'amogh' }).exec())._id
+  })
   order.save()
     .then((result) => { res.status(200).json({message: 'order placed', result: result}) })
     .catch((err) => { res.status(500).json({message: 'order not placed', err: err}) })
