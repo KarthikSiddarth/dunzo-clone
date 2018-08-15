d5f80806f71dd25b2355e1393f8b945f5b28046a
KarthikSiddarth <iamkarthiksiddarth@outlook.com>
2018-08-16 00:07:47 +0530

    add functions to post and show assigned orders

--- view/public/functions.js
+++ view/public/functions.js
@@ -22,8 +22,13 @@ function placeOrderFunction () {
 }
 
 function showOrdersFunction () {
-  const url = 'http://localhost:8000/api/orders/placed'
-  fetch(url).then(data => data.json()).then((orders) => { this.placedOrders = orders })
+  let url
+  if (this.$route.path === '/') {
+    url = 'http://localhost:8000/api/orders/'
+  } else if (this.$route.path === '/runner') {
+    url = 'http://localhost:8000/api/orders/placed'
+  }
+  fetch(url).then(data => data.json()).then((orders) => { this.placedOrders = orders; console.log(orders) })
 }
 
 function getStatusFunction (response) {
@@ -33,4 +38,18 @@ function getStatusFunction (response) {
     return
   }
   this.orderPlacementStatus = 'your order did not get place :('
+}
+
+function assignOrderFunction (order) {
+  const url = 'http://localhost:8000/api/runners/5b73f30faf84a74dc0ef8adf'
+  let postAssignmentOptions = getPostAssignmentOptions(order)
+  fetch(url, postAssignmentOptions).then(data => data.json()).then((res) => { console.log(res) })
+}
+
+function getPostAssignmentOptions (order) {
+  return {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(order)
+  }
 }
