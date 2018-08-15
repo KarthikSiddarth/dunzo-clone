67933d96c59fbf1a345aad6fff0084e7226a04ce
KarthikSiddarth <iamkarthiksiddarth@outlook.com>
2018-08-15 16:43:39 +0530

    add p tag to display warning

--- view/public/vueComponents.js
+++ view/public/vueComponents.js
@@ -1,6 +1,8 @@
 const placeOrderOptions = {
   template: `<div>
-                <input type="text" placeholder="Place your order">
-                <button>place</button>
-             </div>`
+                <input type="text" placeholder="Place your order" @input="$emit('input', $event.target.value)">
+                <p v-if="showNoOrderWarning">No order has been described</p>
+                <button @click="$emit('order')">place</button>
+             </div>`,
+  props: ['showNoOrderWarning']
 }
