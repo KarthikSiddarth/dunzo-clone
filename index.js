b4de5060715ec9d701cd1f6db6716be857fcff93
Mukul Mahajan Singraur <rammmukul@outlook.com>
2018-08-15 13:01:44 +0530

    add static parts

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const orderRoute = require('./api/routes/orders')
+const path = require('path')
 
+app.use('/', express.static(path.join(__dirname, 'view', 'public')))
 app.use(bodyParser.json())
-app.use('/orders', orderRoute)
+app.use('/api/orders', orderRoute)
 
 app.listen(8000, () => console.log('listening on port 8000'))
