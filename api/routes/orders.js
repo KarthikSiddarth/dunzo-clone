a1e4ac115954bab2534deb756ed27d02317a2c64
Mukul Mahajan Singraur <rammmukul@outlook.com>
2018-08-15 16:46:15 +0530

    add update route to orders

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -29,4 +29,14 @@ router.post('/', async (req, res) => {
   }
 })
 
+router.put('/:id', async (req, res) => {
+  const orderId = req.params.id
+  try {
+    let result = await Order.update({_id: orderId}, {$set: req.body})
+    res.status(200).json({message: 'order updated', result})
+  } catch (error) {
+    res.status(500).json({message: 'order not updated', error})
+  }
+})
+
 module.exports = router
