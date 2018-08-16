aac3d6a6e78152659354758281980a91e98776b9
KarthikSiddarth <iamkarthiksiddarth@outlook.com>
2018-08-16 17:17:05 +0530

    remove useless properties and functions

--- view/public/runners/script.js
+++ view/public/runners/script.js
@@ -1,26 +1,11 @@
-Vue.component('place-order', placeOrderOptions)
-Vue.component('show-orders', showPlacedOrdersOptions)
-const userView = Vue.component('user-view', userViewOptions)
-const runnerView = Vue.component('runner-view', runnerViewOptions)
-Vue.component = Vue.component('assigned-order', showAssignedOrderOptions)
-
-const router = new VueRouter({
-  routes: [
-    { path: '/runner', component: runnerView },
-    { path: '/', component: userView }
-  ]
-})
-
 const appOptions = {
-  template: `<div>
-                <ul>
-                  <li><router-link to="/runner">show runner screen</router-link></li>
-                  <li><router-link to="/">show user screen</router-link></li>
-                </ul>
-                <router-view></router-view>
-             </div>`,
   el: '#app',
-  router
+  data: {
+    assignedOrder: []
+  },
+  methods: {
+    getAssignments: getAssignmentsFunction
+  }
 }
 
 const app = new Vue(appOptions)
